7e17498ffcce114efc090d2a82a98c46a72e6cf8
Irfan Budi Prakoso <61933506+irfanbudi24@users.noreply.github.com>
2020-03-10 14:18:19 +0700

    Add files via upload

---
+++ vietgram-master/search.php
@@ -0,0 +1,21 @@
+<?php
+
+	require_once 'koneksi.php';
+
+	function query($query) {
+
+    global $koneksi;
+    $result = mysqli_query($koneksi, $query);
+    $rows = [];
+        while ($row = mysqli_fetch_assoc($result)) {
+            $rows[] =  $row;
+        }
+    return $rows;
+	}
+
+	function cariCaption($cari){
+
+		$query = "SELECT * FROM foto WHERE caption LIKE '%$cari' ";
+		return query($query);
+	}
+?>
